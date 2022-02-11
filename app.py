c5b093138650c4c6e2e03b31e4b98891bc3f16fa
Dennis Au Yeung <ikouhaha888@gmail.com>
2022-02-11 15:34:42 +0800

    init

--- app.py
+++ app.py
@@ -5,12 +5,24 @@ import argparse
 
 from warnings import catch_warnings
 import torch
-from flask import Flask, request
+from flask import Flask, request, make_response,jsonify,Response
 from PIL import Image
 import json
 import io
 
 app = Flask(__name__)
+
+def createJsonResponse(code,result):
+     data = {}
+    #  resp.status_code = code
+     data["result"] = result
+     if code == 200:
+        data["success"] = True
+     else:
+        data["success"] = False
+     response = make_response(data, code)
+     response.headers["Content-Type"] = 'application/json'
+     return response
 
 @app.route("/")
 def hello():
@@ -19,9 +31,10 @@ def hello():
 @app.route("/pet_face_detect", methods=["POST"])
 def predict():
     
-    response = {}
+    response = None
+ 
     try:
-        
+        model = torch.hub.load('ikouhaha/pet_face_yolov5','custom', path='best.pt', force_reload=False)  # force_reload to recache
         if not request.method == "POST":
             return
 
@@ -30,16 +43,19 @@ def predict():
             image_bytes = image_file.read()
             img = Image.open(io.BytesIO(image_bytes))
             results = model(img) 
-            response["data"] = json.loads(results.pandas().xyxy[0].to_json(orient="records"))
-            response["success"] = True
+            resultJson = json.loads(results.pandas().xyxy[0].to_json(orient="records"))
+            response = createJsonResponse(200,resultJson)
         else:
-            response["success"] = False
+            response = createJsonResponse(500,"no image")
+            
+            
     except Exception as e:
-       response["success"] = False
-       response["errMsg"] = str(e)
+       response = createJsonResponse(500,str(e))
+    finally:
+       return response
     
 
-    return response
+   
 
 
 if __name__ == "__main__":
@@ -47,5 +63,5 @@ if __name__ == "__main__":
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
 
-    model = torch.hub.load('ikouhaha/pet_face_yolov5','custom', path='best.pt', force_reload=True)  # force_reload to recache
+    #model = torch.hub.load('ikouhaha/pet_face_yolov5','custom', path='best.pt', force_reload=True)  # force_reload to recache
     app.run(port=args.port)  # debug=True causes Restarting with stat
